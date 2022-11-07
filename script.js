8d3fb32d14068769c0e466b85c3971e39917c311
Jaskirat <jaskiratsaggu1@gmail.com>
2022-11-07 16:24:12 -0700

    created a dropdown for the taal selection

--- script.js
+++ script.js
@@ -4,29 +4,27 @@ let input = document.querySelector("input");
 input.addEventListener("keyup", (event) => {
     event.preventDefault()
     if (event.key === "Enter"){
-        getComposition()
+        customBols()
     }
 })
 
-<<<<<<< HEAD
+// looks for user clicking the preset taal 'GO!' button
 let btn = document.querySelector('#btn');
-let taal = document.querySelector('#taal');
 btn.onclick = (event) => {
     event.preventDefault();
-    alert(taal.value);
+    getTaal();
 };
-=======
 
-
->>>>>>> 92b770655d1e19e502174ca404ade9a92f3ea59f
 /*
-    This function stores a random bol in "bol_string", which index.html prints out
+    This function creates a random bol from the given number of matras and 
+    divides it with the given division variable. 
     Inputs:
         Matra - an int which holds the number of matras that the user wants
+        Division - how the composition will be broken up when it is printed
     Returns:
         Bol_string - a string which holds the random composition
 */
-function getComposition(){
+function getComposition(matra, division){
     bol_array = [
                 'Dha', 'Ta', 
                 'Dhun', 'Tun',
@@ -39,8 +37,7 @@ function getComposition(){
                 'S', 
                 'Ti'];
     var num_bols = bol_array.length;                            // number of bols
-    var matra = document.getElementById("matras").value;        // get number of matras from index.html
-    var bol_string = " ";                                       // initialize the string to be printed 
+    var bol_string = "<br>";                                       // initialize the string to be printed 
 
     // this loop gets two random bols and joins them together to form a matra
     for (var i = 0; i < matra; i++){
@@ -51,12 +48,54 @@ function getComposition(){
         var random_int = Math.floor(Math.random() * num_bols);  // gets random int bw 0 and number of bols - 1
         bol = bol_array[random_int];
         bol_string = bol_string + bol + " ";
+
+        // if we are at the division stage, print a new line
+        if (i == division - 1){
+            bol_string = bol_string + "<br>";
+        }
     }
     document.getElementById("bol_string").innerHTML = bol_string;   // return bol_string
 
 }
 
+/*
+    This function handles the user input into the custom matra field 
+    and then calls getComposition() to handle the creating of the composition
+    Inputs
+        matra - an int from the website input
+*/
+function customBols(){
+    var matra = document.getElementById("matras").value;        // get number of matras from index.html
+    division = Math.floor(matra / 2);
+    getComposition(matra, division);        
+
+}
+
+/*
+    This function handles the user input when they use the dropdown
+    menu and click the button beside it. This function handles
+    the division of each of the taals in the dropdown. 
+    Inputs
+        taal - the value of the dropdown selected by the user from the website
+*/
 function getTaal(){
-    var taal = document.querySelector('#taal');
-    alert(taal);
+    let taal = document.getElementById('taal').value;       // gets taal from index.html
+
+    switch(taal) {
+        case 16:
+            division = 4;
+            break;
+        case 7:
+            division = 7;
+            break;
+        case 4:
+            division = 4;
+        break;
+            case 3:
+            division = 3;
+        break;
+        default:
+            division = taal / 2;
+      }
+    getComposition(taal, division);       
 }
